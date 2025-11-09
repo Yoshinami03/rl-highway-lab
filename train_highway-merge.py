477d469fb80ef1937194b9cb322897695ce28188
minorscarf <104010115+minorscarf@users.noreply.github.com>
2025-11-09 22:26:22 +0900

    add: PPOを利用して学習をする仕組みの追加
    train_highway-merge.pyを追加した
    SupersuitでHighwayMultiEnvをSB3互換形式(VecEnv)に変換
    全エージェントが同一ネットワーク重みを共有して行動
    各エージェントの観測から共通ポリシーを通して勾配を更新
    学習結果は1つの重みファイル(shared_highway_policy.zip)として保存される
    
    NOTE🗒️
    Supersuit PettingZooのマルチエージェント環境を強化学習ライブラリが扱える形に変換してくれるライブラリ集
    
    PPO オンポリシーの方策勾配法で古い方策から大きく外れないように更新をクリップして安定性と実装容易性を両立していて連続・離散学習どちらでも利用できる

---
+++ train_highway-merge.py
@@ -0,0 +1,23 @@
+import supersuit as ss
+from pettingzoo.utils import parallel_to_aec
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from highway_env import HighwayMultiEnv
+
+def make_env(num_agents):
+    env = HighwayMultiEnv(num_agents=num_agents)
+    env = parallel_to_aec(env)
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 4, num_cpus=4, base_class="stable_baselines3")
+    return env
+
+if __name__ == "__main__":
+    vec_env = make_env(num_agents=5)
+
+    model = PPO("MlpPolicy", vec_env, verbose=1, n_steps=1024, batch_size=256, learning_rate=3e-4)
+
+    model.learn(total_timesteps=200000)
+
+    model.save("highway-merge-ppo")
+
+    vec_env.close()
