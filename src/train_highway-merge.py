14964b5bee402cbe9cf0bc37eaba50782dd3255a
Yoshinami03 <104010115+minorscarf@users.noreply.github.com>
2025-12-07 18:21:26 +0900

    clean: 不要コメント削除

--- src/train_highway-merge.py
+++ src/train_highway-merge.py
@@ -39,7 +39,6 @@ def make_env(
     
     env = HighwayMultiEnv(config=config)
     env = ss.pettingzoo_env_to_vec_env_v1(env)
-    # concat_vec_envs_v1は環境から自動的に観測空間と行動空間を取得するため、明示的に渡す必要はない
     env = ss.concat_vec_envs_v1(
         env, 
         num_vec_envs, 
